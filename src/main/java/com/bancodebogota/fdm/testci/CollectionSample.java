dff4cca9d7b8b59a008e06cef0d42206369e69af
mateohernandezs <30628835+mateohernandezs@users.noreply.github.com>
2018-08-02 18:55:07 -0500

    TG-1 Cambios
    asdasd

--- src/main/java/com/bancodebogota/fdm/testci/CollectionSample.java
+++ src/main/java/com/bancodebogota/fdm/testci/CollectionSample.java
@@ -1,6 +1,7 @@
 /*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
+ * Holaaaaa
  * and open the template in the editor.
  */
 package com.bancodebogota.fdm.testci;
